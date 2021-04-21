f1800ad73b6843b45b993ec78b40f6102c1aac14
Carath <florian.sananes@laposte.net>
2021-04-21 22:22:41 +0200

    Added shifting and rescaling of drawings

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -4,6 +4,8 @@
 
 window.onload = function() {
 	// Settings:
+	const borderRatio = 0.05; // param which must be a metadata, as long with width / height (unless in unit square).
+
 	var lineThickness = 6;
 	var samplesSize = 3;
 	var samplesOpacity = 0.5;
@@ -29,7 +31,9 @@ window.onload = function() {
 
 	var showSamplesButton = document.getElementById('showSamplesButton');
 	showSamplesButton.addEventListener("click", function(e) {
-		showSamples(strokes);
+		showSamples(strokes, samplesColor);
+		let strokesInFrame = shiftedAndRescaled(strokes);
+		showSamples(strokesInFrame, 'red');
 	});
 
 	// Starting from the canvas only, but drawing and samples
@@ -59,7 +63,6 @@ window.onload = function() {
 		let bounds = canvas.getBoundingClientRect();
 		currCoord.x = event.clientX - bounds.left;
 		currCoord.y = event.clientY - bounds.top;
-		// console.log(currCoord);
 	}
 
 	function isInCanvas(coord) {
@@ -88,7 +91,7 @@ window.onload = function() {
 
 	function drawDot(dot, size, color) {
 		ctx.beginPath();
-		ctx.arc(dot.x, dot.y, size, 0, 2 * Math.PI, false);
+		ctx.arc(dot.x, dot.y, size, 0, 2. * Math.PI, false);
 		ctx.fillStyle = color; // center
 		ctx.fill();
 		ctx.lineWidth = 1;
@@ -103,16 +106,68 @@ window.onload = function() {
 		currentStroke = [];
 	}
 
-	function showSamples(strokes) {
+	function showSamples(strokes, color) {
 		// clear();
 		ctx.globalAlpha = samplesOpacity;
 		for (let i = 0; i < strokes.length; ++i) {
 			console.log("strokes["+i+"]:", strokes[i]);
 			for (let j = 0; j < strokes[i].length; ++j) {
-				drawDot(strokes[i][j], samplesSize, samplesColor);
+				drawDot(strokes[i][j], samplesSize, color);
 			}
 		}
 		ctx.globalAlpha = 1.0;
+	}
+
+	function boundingBox(strokes) {
+		if (strokes.length == 0) {
+			console.log("Cannot find the bounding box without strokes!");
+			return null;
+		}
+
+		// Starting from the fit dot, which must exist:
+		let xMin = strokes[0][0].x, xMax = strokes[0][0].x;
+		let yMin = strokes[0][0].y, yMax = strokes[0][0].y;
+
+		for (let i = 0; i < strokes.length; ++i) {
+			for (let j = 0; j < strokes[i].length; ++j) {
+				let x = strokes[i][j].x, y = strokes[i][j].y;
+				xMin = x < xMin ? x : xMin;
+				xMax = x > xMax ? x : xMax;
+				yMin = y < yMin ? y : yMin;
+				yMax = y > yMax ? y : yMax;
+			}
+		}
+
+		return [xMin, xMax, yMin, yMax];
+	}
+
+	// Works, but needs to be changed: the strokes should not be stretched...
+	// Careful: integer roundings necessary? If so use Math.round()
+	function shiftedAndRescaled(strokes) {
+		let box = boundingBox(strokes);
+		if (box == null) {
+			return [];
+		}
+
+		let xMin = box[0], xMax = box[1], yMin = box[2], yMax = box[3];
+		let boxWidth = xMax - xMin, boxHeight = yMax - yMin;
+		let canvasDim = Math.min(canvas.width, canvas.height), boxDim = Math.max(boxWidth, boxHeight);
+		let scale = (1. - 2. * borderRatio) * canvasDim / boxDim;
+		let offsetX = scale * ((boxDim -  boxWidth) / 2. - xMin) + (canvas.width  - canvasDim) / 2. + borderRatio * canvasDim;
+		let offsetY = scale * ((boxDim - boxHeight) / 2. - yMin) + (canvas.height - canvasDim) / 2. + borderRatio * canvasDim;
+		let newStrokes = [];
+
+		for (let i = 0; i < strokes.length; ++i) {
+			let stroke = [];
+			for (let j = 0; j < strokes[i].length; ++j) {
+				stroke.push({
+					x: scale * strokes[i][j].x + offsetX,
+					y: scale * strokes[i][j].y + offsetY
+				});
+			}
+			newStrokes.push(stroke);
+		}
+		return newStrokes;
 	}
 
 	function save() {
